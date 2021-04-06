ee891095ebf3303d4c029ad4cc8e072b0093d594
Lucas-Zamora-F <Lzamora@miuandes.cl>
2021-04-06 18:15:43 -0400

    V2

--- main.py
+++ main.py
@@ -3,20 +3,27 @@
 
 
 def main():
-
     while True:
-        number_of_players = input('How many players are going to play?(3 or 4) : ')
+        while True:
+            number_of_players = input('How many players are going to play?(3 or 4) : ')
 
-        try:
-            if int(number_of_players):
-                break
-        except ValueError:
+            try:
+                if int(number_of_players):
+                    break
+            except ValueError:
+            
+                print('Insert a valid number of players!')
+                print()
+        
+        n_players = int(number_of_players)
+        
+        if n_players == 3 or n_players == 4:
+            break
+        else:
             print('Insert a valid number of players!')
-    
-    n_players = int(number_of_players)
-    print(n_players)
+            print()
 
-    
+        
 
 
 if __name__ == "__main__":
