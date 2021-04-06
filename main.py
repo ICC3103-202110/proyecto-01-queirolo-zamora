e8b52398866e09ccfeac57537897671995d94e8b
Lucas-Zamora-F <Lzamora@miuandes.cl>
2021-04-06 18:12:37 -0400

    v1

--- main.py
+++ main.py
@@ -1,2 +1,23 @@
-a = 1
-Prueba = 2
+
+
+
+
+def main():
+
+    while True:
+        number_of_players = input('How many players are going to play?(3 or 4) : ')
+
+        try:
+            if int(number_of_players):
+                break
+        except ValueError:
+            print('Insert a valid number of players!')
+    
+    n_players = int(number_of_players)
+    print(n_players)
+
+    
+
+
+if __name__ == "__main__":
+    main()
