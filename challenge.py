cc1ca3440738c99c93d50a85245b889d72384e5d
Lucas-Zamora-F <Lzamora@miuandes.cl>
2021-04-08 22:10:43 -0400

    counterattack

--- challenge.py
+++ challenge.py
@@ -3,26 +3,29 @@ from abc import abstractmethod
 
 class Challenge (ABC):
 
-    def __init__(self, victim, challenged_card, n_players):
+    def __init__(self, victim, challenged_card, n_players, players):
         self.victim = victim
         self.challenged_card = challenged_card
         self.n_players = n_players
+        self.players = players
     
     def challenge(self):
         y_n = input('someone whats to challenge this action?(yes/no)')
         if  y_n == 'yes':
-            for i in self.n_players
-                if players[i] != self.victim:
-                    print(i+1, '.-', players[i])
+            for i in self.n_players:
+                if self.players[i] != self.victim:
+                    print(i+1, '.-', self.players[i])
                 
             attacker = (int(input()))-1
 
-            if challenged_card == self.victim.cards[0] or challenged_card == self.victim.cards[1]:
-                print(players[attacker], 'lose the challenge')
-                print(players[attacker], 'chose a card to reval!')
-                print('1.-', players[attacker].cards[0])
-                print('2.-', players[attacker].cards[1])
+            if self.challenged_card == self.victim.cards[0] or self.challenged_card == self.victim.cards[1]:
+                print(self.players[attacker], 'lose the challenge')
+                print(self.players[attacker], 'chose a card to reval!')
+                print('1.-', self.players[attacker].cards[0])
+                print('2.-', self.players[attacker].cards[1])
                 chosen_card = input()
+                
+                return 'victim'
 
             else:
                 print(self.victim, 'lose the challenge')
@@ -30,6 +33,10 @@ class Challenge (ABC):
                 print('1.-', self.victim.cards[0])
                 print('2.-', self.victim.cards[1])
                 chosen_card = input()
+
+                return 'attacker'
+
+
 
 
         elif y_n == 'no':
--- counterattack.py
+++ counterattack.py
@@ -5,20 +5,25 @@ from challenge import Challenge
 
 class Counterattack(ABC):
 
-    def __init__(self, victim, counterattack_card, n_players):
+    def __init__(self, victim, counterattack_card, n_players, players):
         self.victim = victim
         self.counterattack_card = counterattack_card
         self.n_players = n_players
+        self.players = players
 
     def counterattack(self):
         y_n = input('someone whats to challenge this counterattack?(yes/no)')
         if  y_n == 'yes':
-            print('who is counterattacking?')
-            print('1.' )
-            print('2.' )
-            if self.n_players == 4:
-                print('3.' )
+            for i in self.n_players:
+                if self.players[i] != self.victim:
+                    print(i+1, '.-', self.players[i])
             attacker = input()
-            Challenge.challenge(Challenge(attacker, self.counterattack_card))
+            winner = Challenge.challenge(Challenge(attacker, self.counterattack_card, self.players))
+
+            if winner == 'victim':
+                return True
+            if winner == 'attacker':
+                return False
+
         elif y_n == 'no':
             return True
