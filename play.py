56cbff7f183fa541539e703b8cf287d850d86ffd
jostomasqueirolo <jtqueirolo@miuandes.cl>
2021-04-09 19:12:45 -0400

    Acciones parcialmente hechas

--- play.py
+++ play.py
@@ -4,7 +4,7 @@ import time
 from showgame import Show_game
 from players import Player
 from deck import Deck
-#from counterattack import Counterattack
+from counterattack import Counterattack
 from challenge import Challenge
 
 class Play (ABC):
@@ -25,7 +25,7 @@ class Play (ABC):
 
                 Show_game.show_game(Show_game(self.players[i], self.players, self.n_players))
 
-                print('action you want to perform!')
+                print('Action you want to perform?')
                 print('1. income ')
                 print('2. Foreign aid')
                 print('3. Strike')
@@ -41,14 +41,12 @@ class Play (ABC):
 
                 elif action == 2:
                     if (input('Someone whats to counterattack this action? (yes/no)') == "yes"):
-                        pass
-                        #blocked = Counterattack.counterattack(Player[i], 1)
+                        blocked = Counterattack.counterattack(Player[i], 1)
                     else:
-                        pass
-                        #blocked = False
+                        blocked = False
 
-                    #if bloked == False:
-                        #self.players[i].change_amount_coins(2)
+                    if bloked == False:
+                        self.players[i].change_amount_coins(2)
                    
 
 
@@ -56,19 +54,106 @@ class Play (ABC):
                 elif action == 3:
                     self.players[i].coins-=7
                     print()
-                    print('-7 coins. ')
+                    print('-7 coins.')
+                    print()
                     COUPED=input("Choose a player: ")
-                    if COUPED == self.players[0].name:
-                        pass
+
+                    if self.n_players == 3:
+                        if COUPED == self.players[0].name:
+                            pass
+                        elif COUPED == self.players[1].name:
+                            pass
+                        elif COUPED == self.players[2].name:
+                            pass
+                     if self.n_players == 4:
+                        if COUPED == self.players[0].name:
+                            pass
+                        elif COUPED == self.players[1].name:
+                            pass
+                        elif COUPED == self.players[2].name:
+                            pass
+                        elif COUPED == self.players[3].name:
+                            pass
 
                 elif action == 4:
-                    pass
+                    self.players[i].coins+=3
+                    print()
+                    print("You've earned 3 coins")
+                    print()
 
                 elif action == 5:
-                    pass
+                    if (input('Someone whats to counterattack this action? (yes/no)') == "yes"):
+                        blocked = Counterattack.counterattack(Player[i], 5)    #ver si 5 es condesa
+                    else:
+                        blocked = False
+
+                    if bloked == False:
+                        self.players[i].coins-=3
+                        MUERDERED=input("Choose a player: ")
+
+                        if self.n_players == 3:
+                            if MUERDERED == self.players[0].name:
+                                pass
+                            elif MUERDERED == self.players[1].name:
+                                pass
+                            elif MUERDERED == self.players[2].name:
+                                pass
+                        if self.n_players == 4:
+                            if MUERDERED == self.players[0].name:
+                                pass
+                            elif MUERDERED == self.players[1].name:
+                                pass
+                            elif MUERDERED == self.players[2].name:
+                                pass
+                            elif MUERDERED == self.players[3].name:
+                                pass
 
                 elif action == 6:
-                    pass
+                    if (input('Someone whats to counterattack this action? (yes/no)') == "yes"):
+                        blocked = Counterattack.counterattack(Player[i], 4)
+                    else:
+                        blocked = False
 
+                    if bloked == False:
+                        EXTORTED=input("Choose a player: ")
+
+                        if self.n_players == 3:
+                            if EXTORTED == self.players[0].name:
+                                if self.players[0].coins>=2:
+                                    self.players[0].coins-=2
+                                else:
+                                    self.players[0].coins-=1
+                            elif EXTORTED == self.players[1].name:
+                                if self.players[1].coins>=2:
+                                    self.players[1].coins-=2
+                                else:
+                                    self.players[1].coins-=1
+                            elif EXTORTED == self.players[2].name:
+                                if self.players[2].coins>=2:
+                                    self.players[2].coins-=2
+                                else:
+                                    self.players[2].coins-=1
+                        if self.n_players == 4:
+                            if EXTORTED == self.players[0].name:
+                                if self.players[0].coins>=2:
+                                    self.players[0].coins-=2
+                                else:
+                                    self.players[0].coins-=1
+                            elif EXTORTED == self.players[1].name:
+                                if self.players[1].coins>=2:
+                                    self.players[1].coins-=2
+                                else:
+                                    self.players[1].coins-=1
+                            elif EXTORTED == self.players[2].name:
+                                if self.players[2].coins>=2:
+                                    self.players[2].coins-=2
+                                else:
+                                    self.players[2].coins-=1
+                            elif EXTORTED == self.players[3].name:
+                                if self.players[3].coins>=2:
+                                    self.players[3].coins-=2
+                                else:
+                                    self.players[3].coins-=1
+                
                 elif action == 7:
-                    pass
+                     pass
