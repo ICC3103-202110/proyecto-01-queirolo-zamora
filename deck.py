7fcce2ad5beb1ca2f911cb732494f45e63056552
jostomasqueirolo <jtqueirolo@miuandes.cl>
2021-04-09 19:26:19 -0400

    Función para devolver: hecha

--- deck.py
+++ deck.py
@@ -3,8 +3,9 @@ from abc import abstractmethod
 
 class Deck(ABC):
 
-    def __init__(self, deck):
+    def __init__(self, deck, hand_deck):
         self.deck = deck
+        self.hand_deck = hand_deck
     
     def draw_card_from_deck(self):
 
@@ -12,7 +13,9 @@ class Deck(ABC):
         self.deck.remove[rcard]
         return rcard
 
-    def return_card(self,card):
-        pass
+    def return_card(self,card,hand_deck):
+        self.hand_deck.remove(self.card)
+        self.deck.append(self.card)
+        return self.hand_deck, self.deck
 
 
